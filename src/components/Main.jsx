bd6524d1ef2c27dca2a0ba8f3a458bb61bb372ec
Janne B <janne..berg@myy.haaga-helia.fi>
2017-12-31 17:45:50 +0200

    project created

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Component } from 'react';
-import Footer from './Footer.jsx';
-import Introduction from './Introduction.jsx';
+import Soldier from './Soldier.jsx';
 
 require('../sass/main.scss');
 
 export default class Main extends Component {
   render() {
     return (
-     <section className="portfolio-app-container">
-        <Introduction/>
-        <Footer/>
+     <section className="game-container">
+       <Soldier/>
+
      </section>
       );
   }
